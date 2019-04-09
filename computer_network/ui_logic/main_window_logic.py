1ae38476252f1d63be8a7038c6b484716391008b
golony6449 <bb0162@live.co.kr>
2019-04-09 10:46:12 +0900

    색상, 그리기 모드 설정 UI 구현

--- computer_network/ui_logic/main_window_logic.py
+++ computer_network/ui_logic/main_window_logic.py
@@ -1,6 +1,8 @@
 from ui.main_window import Ui_MainWindow
 from PyQt5.QtWidgets import QMainWindow
 
+from module.const_value import *
+
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
@@ -9,6 +11,45 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setMouseTracking(True)
         self.centralwidget.setMouseTracking(True)
 
+        # 이벤트 관리용 리스트 작성
+        self.action_list = [self.actionCharactor, self.actionPicture, self.actionDraw,
+                            self.actionRed, self.actionBlue, self.actionYellow, self.actionGreen,
+                            self.actionPurple]
+        self.mode = DRAW_SELECTED
+        self.color = BLUE_SELECTED
+
+        self.event_init()
+
+    def event_init(self):
+        # for i, action in enumerate(self.action_list):
+        #     action.toggled.connect(lambda : self.triggerEvent(i))
+
+        self.actionCharactor.triggered.connect(lambda : self.triggerEvent(CHAR_SELECTED))
+        self.actionPicture.triggered.connect(lambda : self.triggerEvent(PICTURE_SELECTED))
+        self.actionDraw.triggered.connect(lambda : self.triggerEvent(DRAW_SELECTED))
+        self.actionRed.triggered.connect(lambda : self.triggerEvent(RED_SELECTED))
+        self.actionBlue.triggered.connect(lambda : self.triggerEvent(BLUE_SELECTED))
+        self.actionYellow.triggered.connect(lambda : self.triggerEvent(YELLOW_SELECTED))
+        self.actionGreen.triggered.connect(lambda : self.triggerEvent(GREEN_SELECTED))
+        self.actionPurple.triggered.connect(lambda : self.triggerEvent(PURPLE_SELECTED))
+
+    def triggerEvent(self, event):
+        print('Event: ', event, " value: ", self.actionCharactor.isChecked())
+
+        # 이벤트 유형 구분
+        if CHAR_SELECTED < event < DRAW_SELECTED:
+            self.mode = event
+        else:
+            self.color = event
+
+        for i, action in enumerate(self.action_list):
+            if i != self.mode and i != self.color:
+                action.setChecked(False)
+            # else:
+            #     print(action, "is Checked")
+            #     action.setChecked(True)
+
+
     def mousePressEvent(self, event):
         pass
 
@@ -16,4 +57,5 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         pass
 
     def mouseMoveEvent(self, event):
-        print('x: ', event.x(), 'y: ', event.y())
+        # print('x: ', event.x(), 'y: ', event.y())
+        pass
